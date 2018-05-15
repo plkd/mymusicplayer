e8df1565f4533111f460a2fd5b906c7546cb24d8
lijiliang <ljlprismo@gmail.com>
2018-05-15 18:46:32 +0800

    init

---
+++ src/pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="input-item">
+            <span class="tip">账号：</span>
+            <input type="text" class="account">
+        </div>
+        <div class="input-item">
+            <span class="tip">密码：</span>
+            <input type="password" class="password">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Log In',
+  data() {
+    return {};
+  },
+  methods: {
+    init() {}
+  }
+};
+</script>
